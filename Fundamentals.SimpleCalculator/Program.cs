00a12aed244ceb96d40567e6fd49915b9135cc2f
nafi.dhe21 <nafi.dhe21@gmail.com>
2023-10-04 22:39:30 +0700

    Simple Calculator

---
+++ Fundamentals.SimpleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number: ");
+
+int firstInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the second number: ");
+
+int secondInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What do you want to do with those number?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+
+string userChoice = Console.ReadLine().ToUpper();
+
+if (userChoice == "A")
+{
+    Console.WriteLine($"{firstInput} + {secondInput} = {firstInput+secondInput}");
+} 
+else if (userChoice == "S")
+{
+    Console.WriteLine($"{firstInput} - {secondInput} = {firstInput - secondInput}");
+}
+else if (userChoice == "M")
+{
+    Console.WriteLine($"{firstInput} * {secondInput} = {firstInput*secondInput}");
+}
+else
+{
+    Console.WriteLine("Invalid Choice!");
+}
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
