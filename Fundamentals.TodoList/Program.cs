a7b7a829025ce7bf7a8ae79f08a620bd3c123f6e
nafi.dhe21 <nafi.dhe21@gmail.com>
2023-10-05 23:29:35 +0700

    Created simple todolist

---
+++ Fundamentals.TodoList/Program.cs
@@ -0,0 +1,74 @@
+﻿List<string> toDos = new List<string>();
+
+while (true)
+{
+    Console.WriteLine("Hello!");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+
+    string userChoice = Console.ReadLine();
+    Console.WriteLine(userChoice);
+
+    if (userChoice == "S" || userChoice == "s")
+    {
+        if (toDos.Count == 0)
+        {
+            Console.WriteLine("To Do List is empty");
+        }
+        else
+        {
+            for (int i = 0; i < toDos.Count; i++)
+            {
+                Console.WriteLine($"{i+1}. {toDos[i]}");
+            }
+        }
+    }
+    else if (userChoice == "A" || userChoice == "a")
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter the TODO description:");
+            string description = Console.ReadLine();
+            bool inList = toDos.Contains(description);
+            if (inList)
+            {
+                Console.WriteLine("The description must be unique.");
+            }
+            else
+            {
+                Console.WriteLine($"TODO successfully added: {description}");
+                toDos.Add(description);
+                break;
+            }
+        }
+    }
+    else if (userChoice == "R" || userChoice == "r")
+    {
+        if (toDos.Count() == 0)
+        {
+            Console.WriteLine("To Do List is empty");
+        }
+        else
+        {
+            Console.WriteLine("Select the index of the TODO you want to remove:");
+            for (int i = 0; i < toDos.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {toDos[i]}");
+            }
+
+            int indexToDelete = int.Parse(Console.ReadLine());
+            Console.WriteLine($"TODO removed: {toDos[indexToDelete-1]}");
+            toDos.RemoveAt(indexToDelete-1);
+        }
+    }
+    else if (userChoice == "E" || userChoice == "e")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice");
+    }
+}
